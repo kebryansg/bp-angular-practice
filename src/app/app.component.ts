bfdff698d703d41ed860d96f652d6af17d088e3d
kebryansg <kebryansg@gmail.com>
2022-01-14 07:32:54 -0500

    Clase-3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+
+  nombreEstudiante: string = 'Kevin Suárez';
+  edadEstudiante: number = 26;
+  horaActual: Date;
+  colors: IColor[] = [
+    {redSocial: 'Whatsapp', color: '#25D366'},
+    {redSocial: 'Facebook', color: '#4267B2'},
+    {redSocial: 'Twitter', color: '#1DA1F2'},
+    {redSocial: 'Youtube', color: '#FF0000'},
+  ];
+
+  mostrarHora(evento: any) {
+    console.log(evento);
+    this.horaActual = new Date();
+    const {target} = evento;
+    target.innerHTML = `Cambio la Hora - ${this.horaActual.getSeconds()}`;
+  }
+
+}
+
+
+interface IColor {
+  redSocial: string;
+  color: string;
 }
